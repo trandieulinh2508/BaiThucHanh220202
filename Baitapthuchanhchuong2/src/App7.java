39bd1beda830635664185a24c8752efb01c39120
trandieulinh2508 <97507080+trandieulinh2508@users.noreply.github.com>
2022-03-05 10:08:05 +0700

    Add files via upload

---
+++ Baitapthuchanhchuong2/src/App7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App7 {
+
+    public static void main(String[] args) {
+
+        for(int i=1;i<=20;i++)
+            System.out.print(i+" ");
+
+
+    }
+
+}
